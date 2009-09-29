b63851bb545f307487c3ad02812f9f466b385afd
steven <steven@86ab09b6-af0f-4ef8-9c8f-963ca3e73f64>
2009-09-29 13:44:03 +0000

    Forgot one thing.

--- branches/iter-steven/div6/src/tau.f
+++ branches/iter-steven/div6/src/tau.f
@@ -5787,7 +5787,7 @@ c...  Check if it is a quasi-double-null grid:
      .        indexiradj)
          GOTO 300
       ELSEIF (buffer(1:16).EQ.'GENERALISED_GRID') THEN
-         IF (sloutput) THEN
+         IF (.FALSE..AND.sloutput) THEN
            CALL ReadGeneralisedGrid_SL(gridunit,ik,ir,rshift,zshift,
      .                                 indexiradj)
          ELSE
