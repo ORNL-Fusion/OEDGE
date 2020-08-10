27d1318d8cd28226023c24812dcf75a74342822b
shawn <13842897+shawnzamperini@users.noreply.github.com>
2020-08-10 19:39:58 +0000

    Still trying to figure out how to put the branch where I want it

--- branches/utk-divimp/utk/comsrc/mod_diagvel.f
+++
@@ -1,6 +0,0 @@
-c     -*-Fortran-*-
-      module mod_diagvel
-         use mod_params
-         implicit none
-         include 'diagvel'
-      end module mod_diagvel
--- branches/utk-divimp/utk/comsrc/mod_hc_global_opts.f
+++
@@ -1,6 +0,0 @@
-c     -*-Fortran-*-
-      module mod_hc_global_opts
-         use mod_params
-         implicit none
-         include 'hc_global_opts'
-      end module mod_hc_global_opts
