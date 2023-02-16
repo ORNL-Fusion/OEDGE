d4c1f808f3943311ba58893ee1e3706bfb9403b2
Shawn Zamperini <shawnzamperini@gmail.com>
2023-02-16 15:10:02 -0800

    Replaced sol29_machhi with the machhi as defined in mod_params.f90

--- comsrc/f90/mod_sol29_input.f90
+++ comsrc/f90/mod_sol29_input.f90
@@ -1,4 +1,5 @@
 module mod_sol29_input
+  use mod_params
   implicit none
   
   ! Variables and arrays related to SOL 29.
@@ -57,10 +58,6 @@ contains
     implicit none
     
     character*(*) :: tag, line
-    real :: sol29_machhi
-    
-    ! Just some arbitrarily high max value. 
-    sol29_machhi = 1.0e37
     
     ! Parameters for the generalized gamma function that defines the
 	! radial velocity distribution of blobs crossing the separatrix.
@@ -70,58 +67,58 @@ contains
 	! https://docs.scipy.org/doc/scipy/reference/generated/scipy.
 	! stats.gengamma.html
     if (tag(1:3).eq.'X01') then
-      call readr(line, vr_gamma_loc, 0.0, sol29_machhi, &
+      call readr(line, vr_gamma_loc, 0.0, machhi, &
         'SOL29: vr_gamma_loc') 
     elseif (tag(1:3).eq.'X02') then
-      call readr(line, vr_gamma_scale, 0.0, sol29_machhi, &
+      call readr(line, vr_gamma_scale, 0.0, machhi, &
         'SOL29: vr_gamma_scale')      
     elseif (tag(1:3).eq.'X03') then
-      call readr(line, vr_gamma_a, 0.0, sol29_machhi, &
+      call readr(line, vr_gamma_a, 0.0, machhi, &
         'SOL29: vr_gamma_a')   
     elseif (tag(1:3).eq.'X04') then
-      call readr(line, vr_gamma_c, 0.0, sol29_machhi, &
+      call readr(line, vr_gamma_c, 0.0, machhi, &
         'SOL29: vr_gamma_c')
     
     ! Analogous to qtim, just using a different variable.
     elseif (tag(1:3).eq.'X05') then
-      call readr(line, timestep, 0.0, sol29_machhi, &
+      call readr(line, timestep, 0.0, machhi, &
         'SOL29: timestep')
     
     ! The starting ne, Te of blobs crossing the separatrix. The values
     ! exponentially decay in time according to the characteristic times
     ! specified (every tau_ne seconds the density decreases by 1/e). 
     elseif (tag(1:3).eq.'X06') then
-      call readr(line, blob_ne, 0.0, sol29_machhi, &
+      call readr(line, blob_ne, 0.0, machhi, &
         'SOL29: blob_ne')   
     elseif (tag(1:3).eq.'X07') then
-      call readr(line, blob_te, 0.0, sol29_machhi, &
+      call readr(line, blob_te, 0.0, machhi, &
         'SOL29: blob_te') 
     elseif (tag(1:3).eq.'X08') then
-      call readr(line, tau_ne, 0.0, sol29_machhi, &
+      call readr(line, tau_ne, 0.0, machhi, &
         'SOL29: tau_ne')  
     elseif (tag(1:3).eq.'X09') then
-      call readr(line, tau_te, 0.0, sol29_machhi, &
+      call readr(line, tau_te, 0.0, machhi, &
         'SOL29: tau_te')
         
     ! Starting target Te to calculate the parallel flows on the first 
     ! iteration.
     elseif (tag(1:3).eq.'X10') then
-      call readr(line, seed_targ_te, 0.0, sol29_machhi, &
+      call readr(line, seed_targ_te, 0.0, machhi, &
         'SOL29: seed_targ_te')
         
     ! Width of the blobs.
     elseif (tag(1:3).eq.'X11') then
-      call readr(line, blob_radius, 0.0, sol29_machhi, &
+      call readr(line, blob_radius, 0.0, machhi, &
         'SOL29: blob_radius')
         
     ! How many blobs to launch and how many iterations to run the 
     ! simulation for. The higher the better here, it will only give 
     ! better statistics and won't change the fundamental results.
     elseif (tag(1:3).eq.'X12') then
-      call readi(line, nblobs, 0, sol29_machhi, &
+      call readi(line, nblobs, 0, machhi, &
         'SOL29: nblobs')  
     elseif (tag(1:3).eq.'X13') then
-      call readi(line, niterations, 0, sol29_machhi, &
+      call readi(line, niterations, 0, machhi, &
         'SOL29: niterations')
         
     ! Whether or not to iterate with EIRENE for the contribution to the
@@ -141,38 +138,38 @@ contains
         
     ! Length of blobs. Assumed constant.
     elseif (tag(1:3).eq.'X17') then
-      call readr(line, blob_length, 0.0, sol29_machhi, &
+      call readr(line, blob_length, 0.0, machhi, &
         'SOL29: blob_length')
     
     ! Measured blob frequency. Assumed constant everywhere.
     elseif (tag(1:3).eq.'X18') then
-      call readr(line, blob_freq, 0.0, sol29_machhi, &
+      call readr(line, blob_freq, 0.0, machhi, &
         'SOL29: blob_freq')
         
     ! Constant offset of the radial velocity. When a value for the blob
     ! is chosen, this value is then added onto it.
     elseif (tag(1:3).eq.'X19') then
-      call readr(line, vr_offset, -sol29_machhi, sol29_machhi, &
+      call readr(line, vr_offset, -machhi, machhi, &
         'SOL29: vr_offset')
         
     ! How many holes (inward moving blobs) to follow as a fraction of
     ! nblobs.
     elseif (tag(1:3).eq.'X20') then
-      call readr(line, frac_holes, 0.0, sol29_machhi, &
+      call readr(line, frac_holes, 0.0, machhi, &
         'SOL29: frac_holes')
     
     ! Similar to the above, just for the holes.
     elseif (tag(1:3).eq.'X21') then
-      call readr(line, hole_ne, -1.0, sol29_machhi, &
+      call readr(line, hole_ne, -1.0, machhi, &
         'SOL29: hole_ne')   
     elseif (tag(1:3).eq.'X22') then
-      call readr(line, hole_te, -1.0, sol29_machhi, &
+      call readr(line, hole_te, -1.0, machhi, &
         'SOL29: hole_te') 
     elseif (tag(1:3).eq.'X23') then
-      call readr(line, hole_tau_ne, -1.0, sol29_machhi, &
+      call readr(line, hole_tau_ne, -1.0, machhi, &
         'SOL29: hole_tau_ne')  
     elseif (tag(1:3).eq.'X24') then
-      call readr(line, hole_tau_te, -1.0, sol29_machhi, &
+      call readr(line, hole_tau_te, -1.0, machhi, &
         'SOL29: hole_tau_te')
         
     ! The starting radial location of the  blobs/holes are generated 
@@ -181,7 +178,7 @@ contains
     ! and then decays from there outwards. This value is mapped to the 
     ! OMP.
     elseif (tag(1:3).eq.'X25') then
-      call readr(line, tau_rad_start, 0.0, sol29_machhi, &
+      call readr(line, tau_rad_start, 0.0, machhi, &
         'SOL29: tau_rad_start')
         
     ! What type of radial velocity distribution to use for the blobs and
@@ -195,10 +192,10 @@ contains
     ! The characteristics of the Gaussian distribution for the blob/hole
     ! vr distribution (when blob_vr_type = 0). 
     elseif (tag(1:3).eq.'X27') then
-      call readr(line, vr_gauss_loc, 0.0, sol29_machhi, &
+      call readr(line, vr_gauss_loc, 0.0, machhi, &
         'SOL29: vr_gauss_loc')
     elseif (tag(1:3).eq.'X28') then
-      call readr(line, vr_gauss_scale, 0.0, sol29_machhi, &
+      call readr(line, vr_gauss_scale, 0.0, machhi, &
         'SOL29: vr_gauss_loc')
         
         
