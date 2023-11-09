3305b8d84fae1cc8dcf63d9ef68f1d9bda0b4836
shawnzamperini <shawnzamperini@gmail.com>
2023-11-09 08:38:49 -0800

    Update conf.py
    Signed-off-by: shawnzamperini <shawnzamperini@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,28 +40,11 @@ exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
-#import sphinx_rtd_theme
+import sphinx_rtd_theme
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
-
-html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
